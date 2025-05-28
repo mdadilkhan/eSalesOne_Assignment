9e2eeb0765b8c363a6473a7f3a6b2aa04538c8c0
mdadilkhan <mdadilakhtar8@gmail.com>
2025-05-28 16:02:20 +0530

    frontend setup done and backend setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline border border-red-500 bg-amber-700">Hello Tailwind</div>
+      <h1 className='bg'>class</h1>
+    </>
+  )
+}
+
+export default App
